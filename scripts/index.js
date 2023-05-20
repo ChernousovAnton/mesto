b069b2e0c4fce03def8e1804009141da452ec973
ChernousovAnton <Chantony_uz@mail.ru>
2023-05-20 12:56:23 +0300

    bugfix: corrections after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 let popup = document.querySelector('.popup');
 let editProfileBtn = document.querySelector('.profile__edit-btn');
 let closePopupBtn = document.querySelector('.popup__close-btn');
-let profileName = document.querySelector('.profile__title');
-let profileText = document.querySelector('.profile__text');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__item_type_name');
-let jobInput = formElement.querySelector('.form__item_type_text');
+let jobInput = formElement.querySelector('.form__item_type_job');
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileText.textContent = jobInput.value;
-  closePopupBtn.click();
+  profileJob.textContent = jobInput.value;
+  closePopudHandler();
 }
 
 function editProfileHandler() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
-  jobInput.value = profileText.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopudHandler() {
