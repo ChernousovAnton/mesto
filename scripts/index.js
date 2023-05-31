d05ce358d622ecba7d2b80b8b763e7283fae6426
ChernousovAnton <Chantony_uz@mail.ru>
2023-05-31 21:58:36 +0300

    bugfix: corrections after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,91 +1,73 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-];
-
-const editProfileBtn = document.querySelector('.profile__edit-btn');
-const addCardBtn = document.querySelector('.profile__add-btn');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
+const btnEditProfileEl = document.querySelector('.profile__edit-btn');
+const btnAddCardEl = document.querySelector('.profile__add-btn');
+const profileNameEl = document.querySelector('.profile__name');
+const profileJobEl = document.querySelector('.profile__job');
 const cardsEl = document.querySelector('.main .elements');
-const editProfilePopup = document.querySelector('#edit-profile-popup');
-const addCardPopup = document.querySelector('#add-card-popup');
-const cardPopup = document.querySelector('#card-popup');
-const closePopupBtns = document.querySelectorAll('.popup__close-btn');
-const editProfileForm = document.querySelector('form[name="edit-profile"]');
-const addCardForm = document.querySelector('form[name="add-card"]');
+const popupEditProfileEl = document.querySelector('#edit-profile-popup');
+const namePopupEditProfileEl = popupEditProfileEl.querySelector('input[name="name"]');
+const jobPopupEditProfileEl = popupEditProfileEl.querySelector('input[name="job"]');
+const popupAddCardEl = document.querySelector('#add-card-popup');
+const popupCardEl = document.querySelector('#card-popup');
+const popupCardFigcaptionEl = popupCardEl.querySelector('.popup__figcaption');
+const popupCardImageEl = popupCardEl.querySelector('.popup__card-image');
+const btnClosePopupEls = document.querySelectorAll('.popup__close-btn');
+const formEditProfileEl = document.querySelector('form[name="edit-profile"]');
+const formAddCardEl = document.querySelector('form[name="add-card"]');
+const templateCard = document.querySelector('#card-template');
 
-function closePopup(evt) {
-  evt.target.closest('.popup').classList.remove('popup_opened');
-}
+
+const closePopup = (e) => e.classList.remove('popup_opened');
+const openPopup = (e) => e.classList.add('popup_opened');
 
 function openEditProfilePopup() {
-  editProfilePopup.classList.add('popup_opened');
-  editProfilePopup.querySelector('input[name="name"]').value = profileName.textContent;
-  editProfilePopup.querySelector('input[name="job"]').value = profileJob.textContent;
-}
-
-function openAddCardPopup() {
-  addCardPopup.classList.add('popup_opened');
+  openPopup(popupEditProfileEl);
+  namePopupEditProfileEl.value = profileNameEl.textContent;
+  jobPopupEditProfileEl.value = profileJobEl.textContent;
 }
 
 function createCard(cardProperty) {
-  const template = document.querySelector('#card-template');
-  const templateClone = template.content.cloneNode(true);
+  const templateClone = templateCard.content.cloneNode(true);
   templateClone.querySelector('.card__title').textContent = cardProperty.name;
-  templateClone.querySelector('.card__image').src = cardProperty.link;
-  templateClone.querySelector('.card__image').alt = cardProperty.name;
+  const cardImage = templateClone.querySelector('.card__image');
+  cardImage.src = cardProperty.link;
+  cardImage.alt = cardProperty.name;
   templateClone.querySelector('.card__trash').addEventListener('click', e => e.target.closest('.card').remove());
   templateClone.querySelector('.card__like').addEventListener('click', e => e.target.classList.toggle('card__like_active'));
-  templateClone.querySelector('.card__image').addEventListener('click', e => {
-    cardPopup.querySelector('.popup__figcaption').textContent = cardProperty.name;
-    cardPopup.querySelector('.popup__card-image').src = cardProperty.link;
-    cardPopup.classList.add('popup_opened');
+  cardImage.addEventListener('click', e => {
+    popupCardFigcaptionEl.textContent = cardProperty.name;
+    popupCardImageEl.src = cardProperty.link;
+    popupCardImageEl.alt = cardProperty.name;
+    openPopup(popupCardEl);
   });
   return templateClone
 }
 
 initialCards.forEach(e => cardsEl.prepend(createCard(e)));
 
-editProfileBtn.addEventListener('click', openEditProfilePopup);
-addCardBtn.addEventListener('click', openAddCardPopup);
-closePopupBtns.forEach(closePopupBtn => closePopupBtn.addEventListener('click', evt => closePopup(evt)));
+btnEditProfileEl.addEventListener('click', openEditProfilePopup);
+btnAddCardEl.addEventListener('click', () => openPopup(popupAddCardEl));
+btnClosePopupEls.forEach(closePopupBtn => closePopupBtn.addEventListener('click', evt => {
+  const popupEl = evt.target.closest('.popup');
+  closePopup(popupEl);
+}));
 
-addCardForm.addEventListener('submit', evt => {
+formAddCardEl.addEventListener('submit', evt => {
   evt.preventDefault();
   const name = evt.target.querySelector('input[name="name"]').value;
   const link = evt.target.querySelector('input[name="link"]').value;
   const cardProperty = {name: name, link: link};
   const newCard = createCard(cardProperty);
+  const popupEl = evt.target.closest('.popup');
   cardsEl.prepend(newCard);
-  closePopup(evt);
+  closePopup(popupEl);
+  evt.target.querySelector('input[name="name"]').value = '';
+  evt.target.querySelector('input[name="link"]').value = '';
 })
 
-editProfileForm.addEventListener('submit', evt => {
+formEditProfileEl.addEventListener('submit', evt => {
   evt.preventDefault();
-  profileName.textContent = evt.target.querySelector('input[name="name"]').value;
-  profileJob.textContent = evt.target.querySelector('input[name="job"]').value;
-  closePopup(evt);
+  profileNameEl.textContent = evt.target.querySelector('input[name="name"]').value;
+  profileJobEl.textContent = evt.target.querySelector('input[name="job"]').value;
+  const popupEl = evt.target.closest('.popup');
+  closePopup(popupEl);
 })
